a77cb6b8d1c3848b5a269257512f91d82fe505a6
Humon-sketch <humbergnyandemo@gmail.com>
2026-01-28 22:50:18 +0300

    Fix formatting issues in image generation code

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // -------------------- CONFIG --------------------
 
-// FUNDAY folder & automatic image generator
+// FUNDAY: images 1 to 300
 const fundayFolder = "FUNDAY";
 const fundayImages = [];
-for(let i=1; i<=300; i++){  // 300 images in FUNDAY
-    let num = i.toString().padStart(4,'0'); // 0001, 0002, ...
+for(let i=1; i<=300; i++){
+    let num = i.toString().padStart(4,'0'); // 0001, 0002...
     fundayImages.push(`IMG-20251204-WA${num}.jpg`);
 }
 
-// PRAYERDAY folder & automatic image generator
+// PRAYERDAY: images 301 to 800
 const prayerdayFolder = "PRAYERDAY";
 const prayerdayImages = [];
-for(let i=301; i<=800; i++){  // 500 images in PRAYERDAY
-    let num = i.toString().padStart(4,'0'); // 0301, 0302, ...
+for(let i=301; i<=800; i++){
+    let num = i.toString().padStart(4,'0'); // 0301, 0302...
     prayerdayImages.push(`IMG-20251204-WA${num}.jpg`);
 }
 
 // -------------------- FUNCTION TO LOAD IMAGES --------------------
 function loadGallery(folder, images, containerId){
     const container = document.getElementById(containerId);
-    images.forEach(imgName => {
+    images.forEach(imgName=>{
         const photoDiv = document.createElement("div");
         photoDiv.className = "photo";
         photoDiv.innerHTML = `
